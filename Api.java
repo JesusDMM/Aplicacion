c9ca53698961db3796ccc983b883237794f72416
Chucho <119381852+JesusDMM@users.noreply.github.com>
2023-08-29 20:11:48 -0500

    Se creo una api con informacion general del clima

--- Api.java
+++ Api.java
@@ -2,14 +2,23 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.text.SimpleDateFormat;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class Api {
 
     public static void main(String[] args) {
         // Reemplaza "TU_CLAVE_DE_API" con tu clave de API real de OpenWeatherMap
         String apiKey = "2fa3fe88c94fe03bd784b101b2253687";
-        String city = "New York"; // Reemplaza con el nombre de la ciudad para la que deseas obtener el clima
-        String apiUrl = "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=1a6a4b01018ba8ffe2386c5df48c0433";
+        String city = "El Mante"; // Reemplaza con el nombre de la ciudad para la que deseas obtener el clima
+        String apiUrl = "http://api.openweathermap.org/data/2.5/forecast?id=3528756&appid=1a6a4b01018ba8ffe2386c5df48c0433&lang=es&units=metric&cnt=24";
 
         try {
             // Crear la URL para la solicitud HTTP
@@ -24,7 +33,7 @@ public class Api {
             int responseCode = conn.getResponseCode();
             if (responseCode == HttpURLConnection.HTTP_OK) {
                 // Leer la respuesta de la API
-                System.out.println("hola");
+                System.out.println("Entro en la api");
                 BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
                 StringBuilder response = new StringBuilder();
                 String line;
@@ -32,10 +41,46 @@ public class Api {
                     response.append(line);
                 }
                 reader.close();
-
                 // Procesar la respuesta JSON
-                System.out.println("Respuesta de la API: ");
-                System.out.println(response.toString());
+                //System.out.println(response.toString());
+                // Parsear el JSON de respuesta
+                JSONObject jsonObject = new JSONObject(response.toString());
+                JSONArray hourlyForecast = jsonObject.getJSONArray("list");
+
+                // Procesar la información de la temperatura por horas
+                for (int i = 0; i < hourlyForecast.length(); i++) {
+                    JSONObject hourData = hourlyForecast.getJSONObject(i);
+                    long timestamp = hourData.getLong("dt");
+                    SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+                    String hora = sdf.format(timestamp*1000);
+                    JSONObject mainData = hourData.getJSONObject("main");
+                    double temperature = mainData.getDouble("temp");
+                    JSONObject fecha = hourData.getJSONObject("sys");
+                    String horario = hourData.getString("dt_txt");
+                    // Convertir el timestamp a hora legible (puedes usar SimpleDateFormat)
+                    // Imprimir la temperatura por horas
+
+                    // Definir el formato de fecha y hora que tiene el String
+            SimpleDateFormat formato = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            
+            // Parsear el String a un objeto Date
+            Date date = formato.parse(horario);
+            
+            // Obtener un objeto Calendar y establecer la fecha del objeto Date
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(date);
+            
+            // Obtener el día y la hora de la fecha
+            int year = calendar.get(Calendar.YEAR);
+            int month = calendar.get(Calendar.MONTH) + 1; // Los meses en Calendar están basados en 0, por lo que se le suma 1 para obtener el valor correcto.
+            int day = calendar.get(Calendar.DAY_OF_MONTH);
+            int hour = calendar.get(Calendar.HOUR_OF_DAY);
+            int minute = calendar.get(Calendar.MINUTE);
+            int second = calendar.get(Calendar.SECOND);
+            String fecha_mostar = day +"/"+ month + "/"+ year;
+            String hora_mostrar = hour + "";
+                    System.out.println("Fecha: " + fecha_mostar + " Hora: " + hora_mostrar + ", Temperatura: " + temperature);
+                }
             } else {
                 // Manejar el error si no se obtiene una respuesta exitosa
                 System.out.println("Error al realizar la solicitud. Código de respuesta: " + responseCode);
